d5f9b951c280126e32bcabc85a43df8f60ca890a
luigi <luigilamastra@gmail.com>
2022-08-11 08:35:37 +0200

    configuration with material-taiwind working

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,36 @@
 import '../styles/globals.css'
+import Script from "next/script";
+import Head from "next/head";
 
+import { ThemeProvider } from "@material-tailwind/react";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+
+  <Head>
+  <link
+  href="https://fonts.googleapis.com/icon?family=Material+Icons"
+  rel="stylesheet"
+/>
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+  integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
+  crossorigin="anonymous"
+/>
+
+  </Head>
+  <Script></Script>
+  <ThemeProvider>
+
+  <Component {...pageProps} />
+
+  </ThemeProvider>
+  
+  </>
+  
+  
+  
+  
 }
 
 export default MyApp
